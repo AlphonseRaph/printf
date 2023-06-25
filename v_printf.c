088fee30952b35c9a464134bddf7101c0a7a7b4a
AlphonseRaph <alphonseoketch2004@gmail.com>
2023-06-25 16:22:20 +0300

    V_printf

---
+++ v_printf.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * v_printf - Custom printf function
+ * @format: The format string
+ * @args: Variable arguments list
+ */
+void v_printf(const char *format, va_list args)
+{
+	int state = 0;
+	char ch;
+	const char *str;
+
+	while (*format)
+	{
+		if (state == 0)
+		{
+			if (*format == '%')
+				state = 1;
+			else
+				putchar(*format);
+		}
+		else if (state == 1)
+		{
+			switch (*format)
+			{
+			case 'c':
+				ch = va_arg(args, int);
+				putchar(ch);
+				break;
+			case 's':
+				str = va_arg(args, const char *);
+				while (*str)
+				{
+					putchar(*str++);
+				}
+				break;
+			}
+			state = 0;
+		}
+
+		format++;
+	}
+}
+		

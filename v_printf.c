53edb9df12a5e695c4a8127c3ec00c00d1a2178d
AlphonseRaph <alphonseoketch2004@gmail.com>
2023-06-27 02:58:50 +0300

    v_printf

--- v_printf.c
+++ v_printf.c
@@ -7,16 +7,17 @@
  * @format: The format string
  * @args: Variable arguments list
  */
-void v_printf(const char *format, va_list args)
+int v_printf(const char *format, va_list args)
 {
 
+	int sum = 0;
 	int state = 0;
 	char ch;
 	const char *str;
 	
 	if (format == NULL)
 	{
-		return;
+		return (-1);
 	}
 	while (*format)
 	{
@@ -25,7 +26,10 @@ void v_printf(const char *format, va_list args)
 			if (*format == '%')
 				state = 1;
 			else
+			{
 				putchar(*format);
+				sum++;
+			}
 		}
 		else if (state == 1)
 		{
@@ -34,12 +38,14 @@ void v_printf(const char *format, va_list args)
 			case 'c':
 				ch = va_arg(args, int);
 				putchar(ch);
+				sum++;
 				break;
 			case 's':
 				str = va_arg(args, const char *);
 				while (*str)
 				{
 					putchar(*str++);
+					sum++;
 				}
 				break;
 			}
@@ -47,4 +53,5 @@ void v_printf(const char *format, va_list args)
 		}
 		format++;
 	}
+	return (sum);
 }

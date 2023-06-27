d09c2a1fedff3ee13d907efe4ba8ad36bdf80945
Jpngeta <jpngeta@gmail.com>
2023-06-28 02:27:00 +0300

    V_printf

--- v_printf.c
+++ v_printf.c
@@ -7,44 +7,54 @@
  * @format: The format string
  * @args: Variable arguments list
  *
- * Return: The length of the output
+ * Return: The number of characters printed
  */
-unsigned int v_printf(const char *format, va_list args)
+int v_printf(const char *format, va_list args)
 {
-	int st = 0;
-	char ch;
-	const char *str;
-	unsigned int leng = 0;
+        int st = 0;
+        char ch;
+        const char *str;
+        int leng = 0;
 
-	while (*format)
-	{
-		if (st == 0)
-		{
-			if (*format == '%')
-				st = 1;
-			else
-				leng += _putchar(*format);
-		}
-		else if (st == 1)
-		{
-			switch (*format)
-			{
-			case 'c':
-				ch = va_arg(args, int);
-				leng += _putchar(ch);
-				break;
-			case 's':
-				str = va_arg(args, const char *);
-				leng += _puts(str);
-				break;
-			case '%':
-				leng += _putchar('%');
-				break;
-			}
-			st = 0;
-		}
+        while (*format)
+        {
+                if (st == 0)
+                {
+                        if (*format == '%')
+                                st = 1;
+                        else
+                        {
+                                putchar(*format);
+                                leng++;
+                        }
+                }
+                else if (st == 1)
+                {
+                        switch (*format)
+                        {
+                        case 'c':
+                                ch = va_arg(args, int);
+                                putchar(ch);
+                                leng++;
+                                break;
+                        case 's':
+                                str = va_arg(args, const char *);
+                                while (*str)
+                                {
+                                        putchar(*str++);
+                                        leng++;
+                                }
+                                break;
+                        case '%':
+                                putchar('%');
+                                leng++;
+                                break;
+                        }
+                        st = 0;
+                }
 
-		format++;
-	}
-	return (leng);
+                format++;
+        }
+        return (leng);
 }
+

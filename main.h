d33c56e0af60982a64668d8d8e910e3e1a5ed170
Jpngeta <jpngeta@gmail.com>
2023-06-28 02:30:10 +0300

    main.h

--- main.h
+++ main.h
@@ -1,12 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
-#include <unstd.h>
 
 int _printf(const char *format, ...);
-unsigned int v_printf(const char *format, va_list args);
-int _puts(const char *str);
-int _putchar(char c);
-unsigned int _strlen(const char *str);
+int v_printf(const char *format, va_list args);
 
 #endif
